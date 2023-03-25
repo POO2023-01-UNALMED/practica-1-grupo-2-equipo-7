54b916c8b6a592e6ff64391911985a9209659275
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-03-24 21:23:34 -0500

    Construcción de la clase grupo

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,3 +1,46 @@
+import java.util.*;
+
 public class Grupo {
-    
+    private int numero;
+    private Profesor profesor;
+    private String horario; 
+    //En esta parte no se que tipo de dato es la variable horario
+    private int cupos;
+    private String salon;
+    private ArrayList<Estudiante> estudiantes;
+
+    public Grupo(int numero, Profesor profesor, String horario, int cupos, String salon){
+      this.numero=numero;
+      this.profesor=profesor;
+      //Otra vez lo mismo, el tipo de dato de horario
+      this.horario=horario;
+      this.cupos=cupos;
+      this.salon=salon;
+      this.estudiantes=new ArrayList<Estudiante>();
+    } 
+
+    public void mostrarInformacionGrupo(){
+      System.out.println("Número del grupo: "+this.numero+", Profesor: "+this.profesor+", Horario: "+this.horario+", Cupos: "+this.cupos+", Salón: "+this.salon);
+    }
+
+    public boolean existenciaEstudiante(Estudiante estudiante){
+      for (int j=0;j<this.estudiantes.size();j++){
+        if (this.estudiantes.get(j)==estudiante){
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public void eliminarEstudiante(Estudiante estudiante){
+      for (int i=0;i<this.estudiantes.size();i++){
+        if (this.estudiantes.get(i)==estudiante){
+          this.estudiantes.remove(i);
+        }
+      }
+    }
+
+    public void agregarEstudiante(Estudiante estudiante){
+      this.estudiantes.add(estudiante);
+    }
 }
