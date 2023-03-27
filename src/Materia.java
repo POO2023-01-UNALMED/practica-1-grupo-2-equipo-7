b98ed165a9d27147864a469af22d1cb606429c16
LibardoNavarro <josenavarrop05@gmail.com>
2023-03-27 12:30:15 -0500

    Atributos, getters y setters clase Materia
    Dejé algunos comentarios con dudas en el código.

--- src/Materia.java
+++ src/Materia.java
@@ -1,3 +1,82 @@
+
+import java.util.ArrayList;
+
 public class Materia {
     
+    //nombre y codigo las declare como constantes. ¿Estan de acuerdo?
+    private final String nombre;
+    private final int codigo;
+    private int creditos;
+    private String facultad;
+    //¿Este atributo cupos si se necesita en esta clase?
+    private int cupos;
+    private ArrayList<Materia> prerrequisitos;
+    private ArrayList<Grupo> grupos;
+
+    public Materia(String nombre, int codigo, int creditos, String facultad, int cupos) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.creditos = creditos;
+        this.facultad = facultad;
+        this.cupos = cupos;
+    }
+
+    public Materia(String nombre, int codigo, int creditos, String facultad, int cupos, ArrayList<Materia> prerrequisitos) {
+        this(nombre, codigo, creditos, facultad, cupos);
+        this.prerrequisitos = prerrequisitos;
+    }
+
+    public Materia(String nombre, int codigo, int creditos, String facultad, int cupos, ArrayList<Materia> prerrequisitos, ArrayList<Grupo> grupos) {
+        this(nombre, codigo, creditos, facultad, cupos, prerrequisitos);
+        this.grupos = grupos;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public int getCodigo() {
+        return this.codigo;
+    }
+    
+    public int getCreditos() {
+        return this.creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getFacultad() {
+        return this.facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public int getCupos() {
+        return this.cupos;
+    }
+
+    public void setCupos(int cupos) {
+        this.cupos = cupos;
+    }
+
+    public ArrayList<Materia> getPrerrequisitos() {
+        return this.prerrequisitos;
+    }
+
+    public void setPrerrequisitos(ArrayList<Materia> prerrequisitos) {
+        this.prerrequisitos = prerrequisitos;
+    }
+
+    public ArrayList<Grupo> getGrupos() {
+        return this.grupos;
+    }
+
+    public void setGrupos(ArrayList<Grupo> grupos) {
+        this.grupos = grupos;
+    }
+     
 }
