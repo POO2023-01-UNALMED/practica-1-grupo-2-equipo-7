023ef17fc95bc138a6c4613c60d78c17a28ecc00
efrain <egomezra@unal.edu.co>
2023-04-07 23:53:22 -0500

    Cambios en coordinador, comienzo de generar horario aleatorio

--- src/gestorAplicacion/usuario/Coordinador.java
+++ src/gestorAplicacion/usuario/Coordinador.java
@@ -1,12 +1,15 @@
 package gestorAplicacion.usuario;
+
+import java.util.ArrayList;
+
+import gestorAplicacion.administracion.*;
+
 public class Coordinador extends Usuario {
     private String facultad;
     private int limitesCreditos;
+    // private static ArrayList<Materia> materiasTotales;
 
     public Coordinador(String facultad,int limitesCreditos,long id, String nombre, String pw) {
-    
-        //podemos ponerlos ahora o despues con el super o con los set del metodo
-        // super(id,nombre,tipo,pw);
         super(id, nombre,pw);
         this.facultad = facultad;
         this.limitesCreditos = limitesCreditos;
@@ -19,38 +22,29 @@ public class Coordinador extends Usuario {
         return "Nombre: "+ getNombre()+ "\nDocumento: "+ getId();
     }
 
-    public void crearMateria(){
-        // Agrega una materia a la "base de datos" pero antes necesitamos saber como funciona la serializacion
-
-        // Faltan los atributos para crear una materia
-
-        Materia materia = new Materia();
+    public void crearMateria(String nombre, int codigo, int creditos){
+        // Generarlo aqui o en el main?
+        Materia materia = new Materia(nombre,codigo, "Sin descripcion", creditos, "Sin facultad", 0);
+        // materiasTotales.add(materia);
     }
 
     public String desmatricular(Estudiante estudiante, Grupo grupo){
-        // Es desmatricular de ya no estar en una materia
+        // Desmatricular no seria simplemente llamar un metodo de estudiante?
+        // Tambien seria que dentro de materias de estudiante no este esta materia
 
         boolean estaMatriculado = grupo.existenciaEstudiante(estudiante);
 
         if (estaMatriculado){
-
             grupo.eliminarEstudiante(estudiante);
             return "El estudiante ha sido desmatriculado de la materia y su respectivo grupo";
-
         }
-
         else{
-
             return "El estudiante no estaba matriculado";
-
         }
-
     }
 
-    public void contratarProfesor(){
-        // Agrega profesor a la base de datos, de nuevo como funcionara?
-
-        Profesor profesor = new Profesor();
+    public void contratarProfesor(String nombre, String facultad){
+        Profesor profesor = new Profesor(nombre,facultad);
     }
 
 
@@ -65,6 +59,46 @@ public class Coordinador extends Usuario {
                 puntero_Estudiante.desmatricularMateria(materia);
             }   
         }
+    }
+
+
+    // ligadura dinamica?
+    public Object[] crearHorarioAleatorio(ArrayList<Materia> materias){
+        /**
+         * Toma una lista de materias que se desean ver.
+         * 
+         * Crear un horario aleatorio en base de los grupos disponibles.
+         * 
+         * Retorna una lista estatica de dos elementos: Un booleano que nos dira si fue posible o no
+         *  crear el horaio y el horario generado.
+         */
+
+        Horario horario;
+
+        Object[] resultado = new Object[2];
+
+        ArrayList<String> totalPosibles;
+
+        boolean ok;
+
+        for (int i=0;i<=materias.size();i++){
+            Materia pMateria = materias.get(i);
+            String posible;
+
+            ArrayList<Grupo> grupos = pMateria.getGrupos();
+            for (int e=0;e<=grupos.size();e++){
+                Grupo pGrupo = grupos.get(e);
+                
+                //trabajo en progreso
+            }
+
+        }
+
+        resultado[0] = ok;
+        resultado[1] = horario;
+
+        return resultado;
+
     }
 
     // Getters
@@ -88,8 +122,5 @@ public class Coordinador extends Usuario {
     public void setLimitesCreditos(int limitesCreditos) {
         this.limitesCreditos = limitesCreditos;
     }
-
-    // FALTAN LO SETTERS Y LOS GETTERS DE LOS ATRIBUTOS QUE VIENE DE USUARIO.
-
 
 }
