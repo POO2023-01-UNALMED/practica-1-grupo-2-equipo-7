31db17702ad93a34ef2197aa4504c0a6813c9ddf
efrain <egomezra@unal.edu.co>
2023-04-09 21:24:09 -0500

    unos mini cambios

--- src/gestorAplicacion/administracion/Salon.java
+++ src/gestorAplicacion/administracion/Salon.java
@@ -11,71 +11,28 @@ public class Salon {
         this.aforo = aforo;
     }
 
-    /*public void agregarGrupo(ArrayList<String> horario,Grupo grupo){
-        for (int i = 0;i<horario.size();i++){
-            
-            // tiene que dar el horario en formato: "1-08-10" "Dia-hora inicio-hora final" todo en horario militar
-            String detalles = horario.get(i);
-            int dia = Integer.parseInt(detalles.substring(0, 1))-1;
-            int horaInicio = Integer.parseInt(detalles.substring(2, 4));
-            int horaFinal = Integer.parseInt(detalles.substring(5, 7))-1;
-            
-            for (int e =horaInicio;e<=horaFinal;e++){
-                this.horario[dia][e] = grupo;
-            }
-
-        }
-    }*/
     
     public void setHorario(Horario horario) {
-    	this.horario = horario;
+        this.horario = horario;
     }
     
     public Horario getHorario() {
-    	return this.horario;
+        return this.horario;
     }
-
+    
 	public String getLugar() {
-		return lugar;
+        return lugar;
 	}
-
+    
 	public void setLugar(String lugar) {
-		this.lugar = lugar;
+        this.lugar = lugar;
 	}
-
+    
 	public int getAforo() {
-		return aforo;
+        return aforo;
 	}
-
+    
 	public void setAforo(int aforo) {
-		this.aforo = aforo;
+        this.aforo = aforo;
 	}
-
-    /*public boolean comprobarDisponibilidadSalon(String hora){
-        
-        int dia = Integer.parseInt(hora.substring(0, 1))-1;
-        int horaInicio = Integer.parseInt(hora.substring(2, 4));
-        int horaFinal = Integer.parseInt(hora.substring(5, 7))-1;
-        
-        boolean boo = true;
-
-        for (int e =horaInicio;e<=horaFinal;e++){
-            if (this.horario[dia][e] != null){
-                boo = false;
-            }
-        }
-        return boo;
-    }
-    */
-
-    // public void verHorario() {
-    //     for(int i=0;i<7;i++){
-    //         System.out.println("");
-    //         System.out.print(i+1+": ");
-    //         for(int e =0;e<24;e++){
-    //             System.out.print((this.horario[i][e]).getSalon());
-    //         }
-    //     }
-    // }
-
 }
--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -1,6 +1,32 @@
 package uiMain;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Prueba");
+        
+
+        int opcion = 3; // input
+        switch(opcion) {
+        case 1:
+            System.out.println("Has seleccionado la opción 1");
+            break;
+        case 2:
+            System.out.println("Has seleccionado la opción 2");
+            break;
+        case 3:
+            System.out.println("Has seleccionado la opción 3");
+            break;
+        case 4:
+            System.out.println("Has seleccionado la opción 3");
+            break;
+        default:
+            System.out.println("Opción inválida");
+            break;
+        }
+
+
+
+
     }
+
+
+    
 }
