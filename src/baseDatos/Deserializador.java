4ece2ead60151f1cac58dccaffe620b1dcdefe53
LibardoNavarro <josenavarrop05@gmail.com>
2023-04-14 23:05:54 -0500

    Deserializador

--- src/baseDatos/Deserializador.java
+++ src/baseDatos/Deserializador.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 import gestorAplicacion.administracion.Grupo;
 import gestorAplicacion.administracion.Horario;
@@ -17,8 +18,33 @@ import gestorAplicacion.usuario.Usuario;
 
 public class Deserializador {
 
-    public static void deserializar(){
+    public static <T> void deserializar(ArrayList<T> lista, String nombre){
         
+        File archivo = new File("");
+        FileInputStream fis;
+        ObjectInputStream ois;
+
+        try {
+
+            File path = new File(archivo.getAbsolutePath()+"/src/baseDatos/temp/"+nombre+".txt");
+            fis = new FileInputStream(path);
+            ois = new ObjectInputStream(fis);
+            lista.addAll((ArrayList<T>) ois.readObject());
+            ois.close();
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+    }
+
+    public static void deserializarListas(){
+        //Clases a deserializar
     }
     
 }
