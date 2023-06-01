9865c36d516ae1c02fa298c147a9eafe1de9bb37
LibardoNavarro <josenavarrop05@gmail.com>
2023-05-31 19:23:44 -0500

    Clases Python

---
+++ Python/src/gestorAplicacion/administracion/Grupo.py
@@ -0,0 +1,107 @@
+# Autores:
+#  MATEO ÁLVAREZ MURILLO
+#  EFRAÍN GÓMEZ RAMÍREZ
+#  LIBARDO JOSÉ NAVARRO PEDROZO
+#  SEBASTIÁN OCAMPO GALVIS
+
+from gestorAplicacion.usuario import Estudiante
+from gestorAplicacion.administracion import Materia, Salon
+
+class Grupo:
+    _gruposTotales = []
+
+    def __init__(self, materia, numero, profesor, horario=[], cupos=None, salon=None):
+        self._materia = materia
+        self._numero = numero
+        self._profesor = profesor
+        self._horario = horario if horario is not None else []
+        self._cupos = cupos
+        self._salon = salon
+        self._estudiantes = []
+        Grupo._gruposTotales.append(self)
+    
+    def mostrarInformacionGrupo(self):
+        return "Número del grupo: {}, Profesor: {}, Horario: {}, Cupos: {}, Salón: {}".format(self.numero, self.profesor, self.horario, self.cupos, self.salon)
+
+    def existenciaEstudiante(self, estudiante):
+        for e in self._estudiantes:
+            if e.getId() == estudiante.getId():
+                return True
+        return False
+
+    def eliminarEstudiante(self, estudiante):
+        indice = -1
+        for i, e in enumerate(self._estudiantes):
+            if e.getNombre() == estudiante.getNombre():
+                indice = i
+                self._cupos += 1
+                estudiante.eliminarGrupo(self)
+                break
+        if indice != -1:
+            self._estudiantes.pop(indice)
+
+    @staticmethod
+    def buscarGrupo(materiaE, grupoE):
+        indicei = -1
+        indicej = -1
+        for i in range(len(Materia.getMateriasTotales())):
+            materia = Materia.getMateriasTotales()[i]
+            if materia.getNombre() == materiaE.getNombre():
+                indicei = i
+                for j in range(len(materia.getGrupos())):
+                    grupo = materia.getGrupos()[j]
+                    if grupo.getNumero() == grupoE.getNumero():
+                        indicej = j
+                        break
+
+        return Materia.getMateriasTotales()[indicei].getGrupos()[indicej
+                                                                 ]
+    def agregarEstudiante(self, estudiante):
+        self._estudiantes.append(estudiante)
+        self._cupos -= 1
+
+    def getNumero(self):
+        return self._numero
+
+    def setNumero(self, numero):
+        self._numero = numero
+
+    def getProfesor(self):
+        return self._profesor
+
+    def setProfesor(self, profesor):
+        self._profesor = profesor
+
+    def getHorario(self):
+        return self._horario
+
+    def setHorario(self, horario):
+        self._horario = horario
+
+    def getCupos(self):
+        return self._cupos
+
+    def setCupos(self, cupos):
+        self._cupos = cupos
+
+    def getSalon(self):
+        return self._salon
+
+    def setSalon(self, salon):
+        self._salon = salon
+
+    def getEstudiantes(self):
+        return self._estudiantes
+
+    def setEstudiantes(self, estudiantes):
+        self._estudiantes = estudiantes
+
+    def getMateria(self):
+        return self._materia
+
+    def setMateria(self, materia):
+        self._materia = materia
+
+    @classmethod
+    def getGruposTotales(cls):
+        return cls._gruposTotales
---
+++ Python/src/gestorAplicacion/usuario/Profesor.py
@@ -0,0 +1,121 @@
+# Autores:
+# EFRAÍN GÓMEZ RAMÍREZ
+# LIBARDO JOSÉ NAVARRO PEDROZO
+# ANA SOFÍA GÓMEZ ZAPATA
+# SEBASTIÁN OCAMPO GALVIS
+
+from gestorAplicacion.administracion import *
+import random
+
+class Profesor:
+    _profesores = []
+
+    def __init__(self, nombre, facultad, materiasDadas, horario = Horario(),grupos=[]):
+        self._nombre = nombre
+        self._facultad = facultad
+        self._materiasDadas = materiasDadas
+        self._horario = horario
+        self._grupos = grupos if grupos is not None else []
+        Profesor._profesores.append(self)
+
+    def vincularGrupo(self, g):
+        self._grupos.append(g)
+        self._horario.ocuparHorario(g.getHorario(), g)
+
+    def desvincularGrupo(self, g):
+        if g in self._grupos:
+            indice = self._grupos.index(g)
+            horaLibre = self._grupos[indice].getHorario()
+            self._horario.liberarHorario(horaLibre)
+            self._grupos.remove(g)
+
+    def daMateria(self, nombre):
+        for materia in self.getMateriasDadas():
+            if materia.getNombre() == nombre:
+                return True
+        return False
+
+    @classmethod
+    def recomendarEstudiante(cls, estudiante):
+        for profesor in cls._profesores:
+            chance = 0
+            suerte = random.randint(1, 10)
+            for grupo in estudiante.getGruposVistos():
+                if grupo.getProfesor().getNombre() == profesor.getNombre():
+                    chance += 5
+                    break
+            if estudiante.getFacultad() == profesor.getFacultad():
+                chance += 3
+            if chance >= suerte:
+                return True
+        return False
+
+    @classmethod
+    def mostrarProfesores(cls):
+        r = ""
+        i = 1
+        for profesor in cls._profesores:
+            r += f"{i}. {profesor.getNombre()}. Materias: "
+            for materia in profesor.getMateriasDadas():
+                if profesor.getMateriasDadas().index(materia) == len(profesor.getMateriasDadas()) - 1:
+                    r += f"{materia.getNombre()}.\n"
+                else:
+                    r += f"{materia.getNombre()}, "
+            i += 1
+        return r
+
+    @classmethod
+    def profesoresDeMateria(cls, nombre):
+        profes = []
+        for profesor in cls._profesores:
+            if profesor.daMateria(nombre) and profesor not in profes:
+                profes.append(profesor)
+        return profes
+    
+    @classmethod
+    def mostrarProfesMateria(cls, nombre):
+        r = ""
+        i = 1
+        profes = cls._profesoresDeMateria(nombre)
+        for profesor in profes:
+            r += f"{i}. {profesor.getNombre()}.\n"
+            i += 1
+        return r
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getFacultad(self):
+        return self._facultad
+
+    def setFacultad(self, facultad):
+        self._facultad = facultad
+
+    def getMateriasDadas(self):
+        return self._materiasDadas
+
+    def setMateriasDadas(self, materiasDadas):
+        self._materiasDadas = materiasDadas
+
+    def getGrupos(self):
+        return self._grupos
+
+    def setGrupos(self, grupos):
+        self._grupos = grupos
+        
+    def setHorario(self, horario):
+        self._horario = horario
+
+    def getHorario(self):
+        return self._horario
+    
+    @classmethod
+    def getProfesores(cls):
+        return cls._profesores
+
+    @classmethod
+    def setProfesores(cls, profesores):
+        cls._profesores = profesores
