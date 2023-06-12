93ae8b61c89b8a7a141c79aec4e3e02a74d8f28f
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-06-12 13:18:53 -0500

    Archivo de matricular materia

--- Python/src/gestorGrafico/ventPrincipal.py
+++ Python/src/gestorGrafico/ventPrincipal.py
@@ -1,32 +1,38 @@
 from tkinter import *
 from tkinter import messagebox
 from DesmatricularAlumno import DesmatricularAlumno
+from MatricularMateria import MatricularMateria
+
 
 class VentPrincipal(Tk):
     def __init__(self):
         super().__init__()
         self.title("Sistema Matricula de Materias")
-        self.resizable(1,1)
+        self.resizable(1, 1)
         self.geometry("865x460")
 
         def infoBasica():
-            messagebox.showinfo("Informacion de la aplicacion","Sistema de matricula de materias")
+            messagebox.showinfo(
+                "Informacion de la aplicacion", "Sistema de matricula de materias"
+            )
 
         def salir():
             self.destroy()
 
         def mostrarAutores():
-            autores = ("Autores de la aplicacion:\n\n" + 
-                       "Mateo Alvarez Murillo\n\n" + 
-                       "Efrain Gomez Ramirez\n\n" + 
-                       "Ana Sofia Gomez Zapata\n\n" + 
-                       "Libardo Jose Navarro Pedrozo\n\n" +
-                       "Sebastian Ocampo Galvis")
+            autores = (
+                "Autores de la aplicacion:\n\n"
+                + "Mateo Alvarez Murillo\n\n"
+                + "Efrain Gomez Ramirez\n\n"
+                + "Ana Sofia Gomez Zapata\n\n"
+                + "Libardo Jose Navarro Pedrozo\n\n"
+                + "Sebastian Ocampo Galvis"
+            )
 
             messagebox.showinfo("Autores", autores)
 
         def matricularMateria():
-            pass
+            MatricularMateria(self).pack()
 
         def generarHorario():
             pass
@@ -42,7 +48,7 @@ class VentPrincipal(Tk):
 
         menuBar = Menu(self)
         self.config(menu=menuBar)
-        
+
         menu1 = Menu(menuBar)
         menuBar.add_cascade(label="Archivo", menu=menu1)
 
@@ -51,10 +57,12 @@ class VentPrincipal(Tk):
 
         menu2 = Menu(menuBar)
         menuBar.add_cascade(label="Procesos y Consultas", menu=menu2)
-        
+
         menu2.add_cascade(label="Matricular materia", command=matricularMateria)
         menu2.add_cascade(label="Generar Horario", command=generarHorario)
-        menu2.add_cascade(label="Eliminar o agregar Materia / Grupo", command=eliminarAgregarMateria)
+        menu2.add_cascade(
+            label="Eliminar o agregar Materia / Grupo", command=eliminarAgregarMateria
+        )
         menu2.add_cascade(label="Desmatricular Alumno", command=desmatricularAlumno)
         menu2.add_cascade(label="Busqueda y Postulacion de Becas", command=becas)
 
@@ -62,8 +70,9 @@ class VentPrincipal(Tk):
         menuBar.add_cascade(label="Ayuda", menu=menu3)
 
         menu3.add_cascade(label="Acerca de", command=mostrarAutores)
-        
+
         self.mainloop()
+
 
 if __name__ == "__main__":
     VentPrincipal()
