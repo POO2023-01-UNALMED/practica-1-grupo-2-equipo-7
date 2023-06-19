457026c79353132358e0f273bbb37446a53b3d17
efrain <efrain200414@gmail.com>
2023-06-19 08:56:58 -0500

    arreglos (de nuevo) en la serializacion

--- Python/src/main.py
+++ Python/src/main.py
@@ -41,42 +41,10 @@ class MainWin(Tk):
 Deserializador.deserializarDatos()
 
 
-# for pHorario in Horario.getHorariosTotales():
-#     print(pHorario.getGrupoContenidos())
-# #     print("\n")
 
-# for pMateria in Materia.getMateriasTotales():
-#     print(pMateria.getGrupos())
 
 MainWin()
-MainWin()
 
-
-
-# c1234 = Estudiante(1234,"Libardo Jose Navarro Pedrozo","Ingenieria de Sistemas e Informatica",3,"Facultad de minas",2,97530865,[c02022_3010438,c102022_3010435,c12022_1000004,c12022_1000005,c22022_1000008,c32022_1000003,c122022_3010334,c182022_1000089,])
-#for pEsutiante in Estudiante.getEstudiantes():
-    # grupos = pEsutiante.getMaterias()
-    # materias = pEsutiante.getGruposVistos()
-    
-    # pEsutiante.setGruposVistos(grupos)
-    # pEsutiante.setMaterias(materias)
-    # if len(grupos)>=1:
-    #     pEsutiante.calcularAvance()
-    #     pEsutiante.calcularPromedio()
-    
-#    print(pEsutiante.getNombre())
-#    print("tiene un promedio de: "+str(pEsutiante.getPromedio())+" y un avance de: "+str(pEsutiante.getAvance()))
-#    print(pEsutiante.getMaterias())
-#    print(pEsutiante.getGruposVistos())
-#    print("\n")
 
 # Serializador.serializarDatos()
 
-# Pruebas desmatricularAlumno
-# for grupo in Estudiante.getEstudiantes()[0].getGrupos():
-#     print(grupo.getMateria().getNombre())
-# print("Estudiantes")
-# for estudiante in Materia.getMateriasTotales()[0].getGrupos()[0].getEstudiantes():
-#     print(estudiante.getNombre())   
-
-# print("salio todo bien bro/sis/helicoptero apache? ;D")
